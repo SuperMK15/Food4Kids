f6c2a9bb37360d8b553c0d22a589a21890f1abee
SuperMK15 <78823812+SuperMK15@users.noreply.github.com>
2021-06-24 12:37:24 -0400

    Update calculator.php

--- pages/calculator.php
+++ pages/calculator.php
@@ -1,72 +1,68 @@
 <?php
-    include_once 'connector.php';
-    $pullingMenuCode = "";
-    $menuNumber = 0;
-    $amountNeeded = 0;
-    if (isset($_POST['stockSubmit'])) {
-      $selectMenu = "SELECT * FROM menus WHERE basketID =" . $_POST['menuNumber'];
-      $selectQuery = mysqli_query($conn, $selectMenu);
-      $selectResult = mysqli_fetch_assoc($selectQuery);
-      for ($x = 1; $x <= 25; $x++) {
-        $stockSelect = "SELECT * FROM items WHERE itemID =" . $selectResult['itemID' . $x];
-        $stockQuery = mysqli_query($conn, $stockSelect);
-        if (mysqli_num_rows($stockQuery) != 0) {
-          $stockRow = mysqli_fetch_assoc($stockQuery);
-          $currentStock = $stockRow['stock'];
-          $newStock = intval($currentStock) - intval($_POST['amountNeeded']);
-          if ($newStock < 0) {
-            $newStock = 0;
-          }
-          $stockSelect = "UPDATE items SET stock = $newStock WHERE itemID =" . $stockRow['itemID'];
-          $stockQueryUpdate = mysqli_query($conn, $stockSelect);
-        }
+include_once 'connector.php';
+$pullingMenuCode = "";
+$menuNumber = 0;
+$amountNeeded = 0;
+if (isset($_POST['stockSubmit'])) {
+  $selectMenu = "SELECT * FROM menus WHERE basketID =" . $_POST['menuNumber'];
+  $selectQuery = mysqli_query($conn, $selectMenu);
+  $selectResult = mysqli_fetch_assoc($selectQuery);
+  for ($x = 1; $x <= 25; $x++) {
+    $stockSelect = "SELECT * FROM items WHERE itemID =" . $selectResult['itemID' . $x];
+    $stockQuery = mysqli_query($conn, $stockSelect);
+    if (mysqli_num_rows($stockQuery) != 0) {
+      $stockRow = mysqli_fetch_assoc($stockQuery);
+      $currentStock = $stockRow['stock'];
+      $newStock = intval($currentStock) - intval($_POST['amountNeeded']);
+      if ($newStock < 0) {
+        $newStock = 0;
       }
+      $stockSelect = "UPDATE items SET stock = $newStock WHERE itemID =" . $stockRow['itemID'];
+      $stockQueryUpdate = mysqli_query($conn, $stockSelect);
     }
-    if (isset($_POST['menu-type-num-submit'])) {
-      //basket1 is noRestrictions
-      //basket2 is halal
-      //basket3 is veg
-      //basket4 is baby
-      //basket5 is other
-      if (isset($_POST['no-restrictions'])) {
-        $menuNumber = 1;
-        $pullingMenuCode = "SELECT * FROM menus WHERE basketID = 1";
-      }
-      elseif (isset($_POST['halal'])) {
-        $menuNumber = 2;
-        $pullingMenuCode = "SELECT * FROM menus WHERE basketID = 2";
-      }
-      elseif (isset($_POST['vegetarian'])) {
-        $menuNumber = 3;
-        $pullingMenuCode = "SELECT * FROM menus WHERE basketID = 3";
-      }
-      elseif (isset($_POST['baby'])) {
-        $menuNumber = 4;
-        $pullingMenuCode = "SELECT * FROM menus WHERE basketID = 4";
-      }
-      else {
-        $menuNumber = 5;
-        $pullingMenuCode = "SELECT * FROM menus WHERE basketID = 5";
-      }
-      if (isset($_POST['bagNum'])) {
-        $amountNeeded = $_POST['bagNum'];
-      }
-    }
-    if (isset($_POST['item1']) AND isset($_POST['calSubmit'])) {
-      $sqlCal = "SELECT * FROM items WHERE identifier ='" . $_POST['item1'] . "'";
-      $resultCal = mysqli_query($conn, $sqlCal);
-    }
-    $tCalories = 0;
-    $tProtein = 0;
-    $tCalcium = 0;
-    $tIron = 0;
-    $tvitaminA = 0;
-    $tvitaminC = 0;
-    $tCarbohydrates = 0;
-    $tSodium = 0;
-    $tSugar = 0;
-    $tFat = 0;
-    $tPrice = 0.00;
+  }
+}
+if (isset($_POST['menu-type-num-submit'])) {
+  //basket1 is noRestrictions
+  //basket2 is halal
+  //basket3 is veg
+  //basket4 is baby
+  //basket5 is other
+  if (isset($_POST['no-restrictions'])) {
+    $menuNumber = 1;
+    $pullingMenuCode = "SELECT * FROM menus WHERE basketID = 1";
+  } elseif (isset($_POST['halal'])) {
+    $menuNumber = 2;
+    $pullingMenuCode = "SELECT * FROM menus WHERE basketID = 2";
+  } elseif (isset($_POST['vegetarian'])) {
+    $menuNumber = 3;
+    $pullingMenuCode = "SELECT * FROM menus WHERE basketID = 3";
+  } elseif (isset($_POST['baby'])) {
+    $menuNumber = 4;
+    $pullingMenuCode = "SELECT * FROM menus WHERE basketID = 4";
+  } else {
+    $menuNumber = 5;
+    $pullingMenuCode = "SELECT * FROM menus WHERE basketID = 5";
+  }
+  if (isset($_POST['bagNum'])) {
+    $amountNeeded = $_POST['bagNum'];
+  }
+}
+if (isset($_POST['item1']) and isset($_POST['calSubmit'])) {
+  $sqlCal = "SELECT * FROM items WHERE identifier ='" . $_POST['item1'] . "'";
+  $resultCal = mysqli_query($conn, $sqlCal);
+}
+$tCalories = 0;
+$tProtein = 0;
+$tCalcium = 0;
+$tIron = 0;
+$tvitaminA = 0;
+$tvitaminC = 0;
+$tCarbohydrates = 0;
+$tSodium = 0;
+$tSugar = 0;
+$tFat = 0;
+$tPrice = 0.00;
 ?>
 
 <!DOCTYPE html>
@@ -79,7 +75,7 @@
   <title>Menu Creator - Food4Kids Organizer - Waterloo Region</title>
   <link rel="shortcut icon" type="image/jpg" href="../images/favicon.png">
 
-  <link href="../styles/theme.css" rel="stylesheet" type="text/css"/>
+  <link href="../styles/theme.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
@@ -122,42 +118,42 @@
           </form>
           <div class="select-menu">
             <?php
-              switch ($menuNumber) {
-                case 1:
-                  echo "<p>No Restrictions Menu</p>";
-                  break;
-                case 2:
-                  echo "<p>Halal Menu</p>";
-                  break;
-                case 3:
-                  echo "<p>Vegetarian Menu</p>";
-                  break;
-                case 4:
-                  echo "<p>Baby Menu</p>";
-                  break;
-                case 5:
-                  echo "<p>Other Menu</p>";
-                  break;
-                default:
-                  echo "<p>Please Select a Menu</p>";
-                  break;
-              }
+            switch ($menuNumber) {
+              case 1:
+                echo "<p>No Restrictions Menu</p>";
+                break;
+              case 2:
+                echo "<p>Halal Menu</p>";
+                break;
+              case 3:
+                echo "<p>Vegetarian Menu</p>";
+                break;
+              case 4:
+                echo "<p>Baby Menu</p>";
+                break;
+              case 5:
+                echo "<p>Other Menu</p>";
+                break;
+              default:
+                echo "<p>Please Select a Menu</p>";
+                break;
+            }
             ?>
           </div>
           <ul>
             <form method="post" action="../pages/calculator.php">
               <datalist id="foods">
                 <?php
-                  if (!isset($_POST['item1'])) {
-                    $sql = "SELECT * FROM items WHERE identifier LIKE '%" . $_POST['item1'] . "%'";
-                    $result = mysqli_query($conn, $sql);
-                    if (mysqli_num_rows($result) != 0) {
-                      while ($row = mysqli_fetch_assoc($result)) {
-                        echo "<option value='". $row['identifier'] . "'></option>";
-                        $counter++;
-                      }
-                    } 
+                if (!isset($_POST['item1'])) {
+                  $sql = "SELECT * FROM items WHERE identifier LIKE '%" . $_POST['item1'] . "%'";
+                  $result = mysqli_query($conn, $sql);
+                  if (mysqli_num_rows($result) != 0) {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                      echo "<option value='" . $row['identifier'] . "'></option>";
+                      $counter++;
+                    }
                   }
+                }
                 ?>
               </datalist>
               <?php
@@ -181,7 +177,7 @@
                   echo "</li>";
                 }
                 echo '<section class="submission">';
-                  echo '<input type="submit" value="Update" class="button" name="calSubmit">';
+                echo '<input type="submit" value="Update" class="button" name="calSubmit">';
                 echo '</section>';
               }
               ?>
@@ -196,8 +192,7 @@
                   $id = $convertRow['itemID'];
                   $updateSelect = "UPDATE menus SET itemID" . $x . " = $id WHERE basketID =" . $_POST['basketNum'];
                   $updateQuery = mysqli_query($conn, $updateSelect);
-                }
-                else {
+                } else {
                   $updateSelect = "UPDATE menus SET itemID" . $x . " = 0 WHERE basketID =" . $_POST['basketNum'];
                   $updateQuery = mysqli_query($conn, $updateSelect);
                 }
@@ -205,15 +200,15 @@
             }
             ?>
             <?php
-              if ($menuNumber != 0) {
-                echo '<form method="post" action="../pages/calculator.php">';
-                  echo '<section class="submission">';
-                    echo '<input type="hidden" name="menuNumber" value="' . $menuNumber . '" >';
-                    echo '<input type="hidden" name="amountNeeded" value="' . $amountNeeded . '" >';
-                    echo '<input type="submit" value="Clear Stock" class="button" name="stockSubmit">';
-                  echo '</section>';
-                echo '</form>';
-              }
+            if ($menuNumber != 0) {
+              echo '<form method="post" action="../pages/calculator.php">';
+              echo '<section class="submission">';
+              echo '<input type="hidden" name="menuNumber" value="' . $menuNumber . '" >';
+              echo '<input type="hidden" name="amountNeeded" value="' . $amountNeeded . '" >';
+              echo '<input type="submit" value="Clear Stock" class="button" name="stockSubmit">';
+              echo '</section>';
+              echo '</form>';
+            }
             ?>
           </ul>
         </div>
@@ -235,70 +230,68 @@
               <th>Amount Needed</th>
             </tr>
             <?php
-              if ($pullingMenuCode == "") {
-                echo "<tr>";
-                echo "<td>Empty</td>";
-                echo "<td>Empty</td>";
-                echo "<td>Empty</td>";
-                echo "<td>Empty</td>";
-                echo "<td>Empty</td>";
-                echo "<td>Empty</td>";
-                echo "<td>Empty</td>";
-                echo "<td>Empty</td>";
-                echo "<td>Empty</td>";
-                echo "<td>Empty</td>";
-                echo "<td>Empty</td>";
-                echo "<td>Empty</td>";
-                echo "<td>Empty</td>";
-                echo "</tr>";
-              }
-              else {
-                $pullingMenuQuery = mysqli_query($conn, $pullingMenuCode);
-                $row = mysqli_fetch_assoc($pullingMenuQuery);
-                for ($x = 1; $x <= 25; $x++) {
-                  if ($row['itemID' . $x] != 0) {
-                    $mitemID = $row['itemID' . $x];
-                    $itemQuery = "SELECT * FROM items WHERE itemID = $mitemID";
-                    $itemResult = mysqli_query($conn, $itemQuery);
-                    if (mysqli_num_rows($itemResult) != 0) {
-                      $itemRow = mysqli_fetch_assoc($itemResult);
-                      echo "<tr>";
-                      echo "<td>" . $itemRow['identifier'] . "</td>";
-                      echo "<td>" . $itemRow['calories'] . "</td>";
-                      echo "<td>" . $itemRow['protein'] . "</td>";
-                      echo "<td>" . $itemRow['calcium'] . "</td>";
-                      echo "<td>" . $itemRow['iron'] . "</td>";
-                      echo "<td>" . $itemRow['vitaminA'] . "</td>";
-                      echo "<td>" . $itemRow['vitaminC'] . "</td>";
-                      echo "<td>" . $itemRow['carbohydrates'] . "</td>";
-                      echo "<td>" . $itemRow['sodium'] . "</td>";
-                      echo "<td>" . $itemRow['sugar'] . "</td>";
-                      echo "<td>" . $itemRow['fat'] . "</td>";
-                      echo "<td>" . floatval($itemRow['price']) . "</td>";
-                      $stockNeeded = intval($amountNeeded) - intval($itemRow['stock']);
-                      if ($stockNeeded > 0) {
-                        echo "<td>" . $stockNeeded . "</td>";
-                      }
-                      else {
-                        echo "<td>None</td>";
-                      }
-                      $tCalories = $tCalories + $itemRow['calories'];
-                      $tProtein = $tProtein + $itemRow['protein'];
-                      $tCalcium = $tCalcium + $itemRow['calcium'];
-                      $tIron = $tIron + $itemRow['iron'];
-                      $tvitaminA = $tvitaminA + $itemRow['vitaminA'];
-                      $tvitaminC = $tvitaminC + $itemRow['vitaminC'];
-                      $tCarbohydrates = $tCarbohydrates + $itemRow['carbohydrates'];
-                      $tSodium = $tSodium + $itemRow['sodium'];
-                      $tSugar = $tSugar + $itemRow['sugar'];
-                      $tFat = $tFat + $itemRow['fat'];
-                      $tPrice = $tPrice + $itemRow['price'];
-                      echo "</tr>";
+            if ($pullingMenuCode == "") {
+              echo "<tr>";
+              echo "<td>Empty</td>";
+              echo "<td>Empty</td>";
+              echo "<td>Empty</td>";
+              echo "<td>Empty</td>";
+              echo "<td>Empty</td>";
+              echo "<td>Empty</td>";
+              echo "<td>Empty</td>";
+              echo "<td>Empty</td>";
+              echo "<td>Empty</td>";
+              echo "<td>Empty</td>";
+              echo "<td>Empty</td>";
+              echo "<td>Empty</td>";
+              echo "<td>Empty</td>";
+              echo "</tr>";
+            } else {
+              $pullingMenuQuery = mysqli_query($conn, $pullingMenuCode);
+              $row = mysqli_fetch_assoc($pullingMenuQuery);
+              for ($x = 1; $x <= 25; $x++) {
+                if ($row['itemID' . $x] != 0) {
+                  $mitemID = $row['itemID' . $x];
+                  $itemQuery = "SELECT * FROM items WHERE itemID = $mitemID";
+                  $itemResult = mysqli_query($conn, $itemQuery);
+                  if (mysqli_num_rows($itemResult) != 0) {
+                    $itemRow = mysqli_fetch_assoc($itemResult);
+                    echo "<tr>";
+                    echo "<td>" . $itemRow['identifier'] . "</td>";
+                    echo "<td>" . $itemRow['calories'] . "</td>";
+                    echo "<td>" . $itemRow['protein'] . "</td>";
+                    echo "<td>" . $itemRow['calcium'] . "</td>";
+                    echo "<td>" . $itemRow['iron'] . "</td>";
+                    echo "<td>" . $itemRow['vitaminA'] . "</td>";
+                    echo "<td>" . $itemRow['vitaminC'] . "</td>";
+                    echo "<td>" . $itemRow['carbohydrates'] . "</td>";
+                    echo "<td>" . $itemRow['sodium'] . "</td>";
+                    echo "<td>" . $itemRow['sugar'] . "</td>";
+                    echo "<td>" . $itemRow['fat'] . "</td>";
+                    echo "<td>" . floatval($itemRow['price']) . "</td>";
+                    $stockNeeded = intval($amountNeeded) - intval($itemRow['stock']);
+                    if ($stockNeeded > 0) {
+                      echo "<td>" . $stockNeeded . "</td>";
+                    } else {
+                      echo "<td>None</td>";
                     }
+                    $tCalories = $tCalories + $itemRow['calories'];
+                    $tProtein = $tProtein + $itemRow['protein'];
+                    $tCalcium = $tCalcium + $itemRow['calcium'];
+                    $tIron = $tIron + $itemRow['iron'];
+                    $tvitaminA = $tvitaminA + $itemRow['vitaminA'];
+                    $tvitaminC = $tvitaminC + $itemRow['vitaminC'];
+                    $tCarbohydrates = $tCarbohydrates + $itemRow['carbohydrates'];
+                    $tSodium = $tSodium + $itemRow['sodium'];
+                    $tSugar = $tSugar + $itemRow['sugar'];
+                    $tFat = $tFat + $itemRow['fat'];
+                    $tPrice = $tPrice + $itemRow['price'];
+                    echo "</tr>";
                   }
                 }
-              } 
-              ?>
+              }
+            }
+            ?>
             <tr>
               <th>Total</th>
               <?php
