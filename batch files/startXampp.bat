21209206a8e14f0648b12c31b8c74517e62985d5
SuperMK15 <78823812+SuperMK15@users.noreply.github.com>
2021-07-01 11:39:40 -0400

    Update startXampp.bat

--- batch files/startXampp.bat
+++ batch files/startXampp.bat
@@ -11,7 +11,7 @@ cd "C:\Program Files\Google\Chrome"
 start chrome.exe http://localhost/food4kids/
 
 cd "C:\xampp"
-timeout /t 3 /nobreak
+timeout /t 4 /nobreak
 taskkill /f /IM xampp-control.exe
 
 exit
